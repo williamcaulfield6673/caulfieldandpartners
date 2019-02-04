503c7fd9e9f6d3af14034ac768dd7e4ed9cc1f7d
williamcaulfield6673 <47324721+williamcaulfield6673@users.noreply.github.com>
2019-02-04 16:50:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@
 	var support = { animations : Modernizr.cssanimations },
 		container = document.getElementById( 'ip-container' ),
 		// header = container.querySelector( 'header.ip-header' ),
-		loader = new PathLoader( document.getElementById( 'ip-loader-circle' ) ),
+		// loader = new PathLoader( document.getElementById( 'ip-loader-circle' ) ),
 		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
 		// animation end event name
 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ];
