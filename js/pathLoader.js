cc6b43f1780da158cba25f848db51546b53651f5
williamcaulfield6673 <47324721+williamcaulfield6673@users.noreply.github.com>
2019-02-04 17:23:16 +0100

    Update pathLoader.js

--- js/pathLoader.js
+++ js/pathLoader.js
@@ -19,11 +19,11 @@
 	}
 
 	PathLoader.prototype._draw = function( val ) {
-		this.el.style.strokeDashoffset = this.el.getTotalLength() * ( 1 - val );
+		//this.el.style.strokeDashoffset = this.el.getTotalLength() * ( 1 - val );
 	}
 
 	PathLoader.prototype.setProgress = function( val, callback ) {
-		this._draw(val);
+		//this._draw(val);
 		if( callback && typeof callback === 'function' ) {
 			// give it a time (ideally the same like the transition time) so that the last progress increment animation is still visible.
 			setTimeout( callback, 200 );
